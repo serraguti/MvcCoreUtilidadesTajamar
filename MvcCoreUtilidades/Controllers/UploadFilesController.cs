5edb3179b03471cfa313f332793c0f412d258981
serraguti <pacoserranox@gmail.com>
2023-03-02 14:26:39 +0100

    Add project files.

---
+++ MvcCoreUtilidades/Controllers/UploadFilesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreUtilidades.Helpers;
+
+namespace MvcCoreUtilidades.Controllers
+{
+    public class UploadFilesController : Controller
+    {
+        private HelperPathProvider helperPath;
+
+        public UploadFilesController(HelperPathProvider helperPath)
+        {
+            this.helperPath = helperPath;
+        }
+
+        public IActionResult SubirFichero()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SubirFichero
+            (IFormFile fichero)
+        {
+            string fileName = fichero.FileName;
+            string path = this.helperPath.MapPath(fileName, Folders.Uploads);
+            using (Stream stream = new FileStream(path, FileMode.Create))
+            {
+                await fichero.CopyToAsync(stream);
+            }
+            ViewData["MENSAJE"] = "Fichero subido a " + path;
+            ViewData["URL"] = "<a href='https://localhost:7172/uploads/batman.jpg'>Mi fichero subido</a>"
+            return View();
+        }
+    }
+}
---
+++ MvcCoreUtilidades/Helpers/HelperPathProvider.cs
@@ -0,0 +1,35 @@
+﻿namespace MvcCoreUtilidades.Helpers
+{
+    public enum Folders { Images = 0, Uploads = 1, Facturas = 2, Temporal = 3}
+
+    public class HelperPathProvider
+    {
+        private IWebHostEnvironment hostEnvironment;
+
+        public HelperPathProvider(IWebHostEnvironment hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        public string MapPath(string fileName, Folders folder)
+        {
+            string carpeta = "";
+            if (folder == Folders.Images)
+            {
+                carpeta = "images";
+            }else if (folder == Folders.Uploads)
+            {
+                carpeta = "uploads";
+            }else if (folder == Folders.Facturas)
+            {
+                carpeta = "facturas";
+            }else if (folder == Folders.Temporal)
+            {
+                carpeta = "temp";
+            }
+            string rootPath = this.hostEnvironment.WebRootPath;
+            string path = Path.Combine(rootPath, carpeta, fileName);
+            return path;
+        }
+    }
+}
