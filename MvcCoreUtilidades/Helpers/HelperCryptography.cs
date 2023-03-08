0aff9400a411e8eb4d24a098537a781f6f27e768
serraguti <pacoserranox@gmail.com>
2023-03-08 10:38:42 +0100

    commit

--- MvcCoreUtilidades/Helpers/HelperCryptography.cs
+++ MvcCoreUtilidades/Helpers/HelperCryptography.cs
@@ -5,6 +5,49 @@ namespace MvcCoreUtilidades.Helpers
 {
     public class HelperCryptography
     {
+        public static string Salt { get; set; }
+
+        private static string GenerateSalt()
+        {
+            Random random = new Random();
+            string salt = "";
+            for (int i = 1; i <= 50; i++)
+            {
+                int aleat = random.Next(0, 255);
+                char letra = Convert.ToChar(aleat);
+                salt += letra;
+            }
+            return salt;
+        }
+
+        public static string EncriptarContenido
+            (string contenido, bool comparar)
+        {
+            if (comparar == false)
+            {
+                //GENERAMOS NUESTRO SALT
+                Salt = GenerateSalt();
+            }
+            //EL SALT LO INCLUIREMOS DONDE DESEEMOS
+            //INCLUIMOS EL SALT AL FINAL DEL PASSWORD A CIFRAR
+            string contenidosalt = contenido + Salt;
+            SHA256 sHA256 = SHA256.Create();
+            byte[] salida;
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            //CONVERTIMOS A BYTES EL CONTENIDO DEL SALT
+            salida = encoding.GetBytes(contenidosalt);
+            //DEBEMOS REALIZAR EL CIFRADO SOBRE CIFRADO N VECES
+            for (int i = 1; i <= 55; i++)
+            {
+                //REALIZAMOS EL CIFRADO
+                salida = sHA256.ComputeHash(salida);
+            }
+            //LIMPIAMOS EL SHA256
+            sHA256.Clear();
+            string resultado = encoding.GetString(salida);
+            return resultado;
+        }
+
         //CREAMOS UN METODO STATIC PARA CIFRAR UN STRING
         //DE FORMA MUY BASICA.  DEVOLVEMOS EL STRING GENERADO
         public static string EncriptarTextoBasico(string contenido)
